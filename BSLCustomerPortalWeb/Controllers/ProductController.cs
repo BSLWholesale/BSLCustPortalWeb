b96cef41ab38f55750be6eefe533180cb0ea5d90
Ankit <banswaraws@gmail.com>
2025-03-26 17:44:11 +0530

    Added Product controller and product class and make Yarn display page

---
+++ BSLCustomerPortalWeb/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using BSLCustomerPortalWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BSLCustomerPortalWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Yarn()
+        {
+            return View();
+        }
+
+        public ActionResult Fabric()
+        {
+            return View();
+        }
+        public ActionResult Garments()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Fn_Get_Yarn_GDType(clsYarn obj)
+        {
+
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCPApiURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/ProductAPI/Fn_Get_Yarn_GDType", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "No data found." }, JsonRequestBehavior.AllowGet);
+
+                }
+            }
+        }
+    }
+}
