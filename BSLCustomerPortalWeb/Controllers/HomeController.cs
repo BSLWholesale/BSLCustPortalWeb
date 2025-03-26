105be1f3a8e46a32c984ffbdd3eaaf714b00ccd4
Kanchan <banswaraws@gmail.com>
2025-03-26 17:10:17 +0530

    Added a Login design in Index.cshtml Page view, added clsCustomer.cs class added, Images folder added, HomeController.cs modified, JS folder added, Web.config, BSLCustomerPortalWeb.csproj [Only commit stage changes] - Kanchan

--- BSLCustomerPortalWeb/Controllers/HomeController.cs
+++ BSLCustomerPortalWeb/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using BSLCustomerPortalWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using System.Net.Http;
+using System.Text;
 
 namespace BSLCustomerPortalWeb.Controllers
 {
@@ -27,11 +31,31 @@ namespace BSLCustomerPortalWeb.Controllers
             return View();
         }
 
-        public ActionResult DashBoard()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+        [HttpPost]
+        public JsonResult Fn_LogIn_Customer(clsCustomer objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCustPortalWebAPI"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/CustomerAPI/Fn_LogIn_Customer", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "No data found." }, JsonRequestBehavior.AllowGet);
+                }
+            }
         }
+
+
     }
 }
