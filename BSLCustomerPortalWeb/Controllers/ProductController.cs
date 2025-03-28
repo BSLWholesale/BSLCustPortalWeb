9c1c1cce50fe5452accc879330f0e986798131d2
Kanchan <banswaraws@gmail.com>
2025-03-28 17:14:45 +0530

    Modified in ProductController.cs controller added Fn_Get_Fabric_CatalogueDetail(), FabricCatalogue.cshtml added with JS method Fn_Get_Fabric_CatalogueDetail() and design added, Images added for Fabric view purpose, modified DashBoard.cshtml that added links for Fabric, BSLCustomerPortalWeb.csproj [Only Commit Stage Changes] - Kanchan

--- BSLCustomerPortalWeb/Controllers/ProductController.cs
+++ BSLCustomerPortalWeb/Controllers/ProductController.cs
@@ -207,5 +207,31 @@ namespace BSLCustomerPortalWeb.Controllers
         }
 
 
+        [HttpPost]
+        public JsonResult Fn_Get_Fabric_CatalogueDetail(clsFabric objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCustPortalWebAPI"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/value"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/ProductAPI/Fn_Get_Fabric_CatalogueDetail", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "No Data Found." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+
+
     }
 }
