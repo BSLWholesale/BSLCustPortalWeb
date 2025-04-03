75535693c49ad24badf24ef02f6d5218b37c27e1
Kanchan <banswaraws@gmail.com>
2025-04-03 18:05:44 +0530

    InquiryController.cs, blocks are added DashBoard.cshtml, page design and and alignment  of DisplayQuotation.cshtml, Quotation.cshtml page added in Inquiry Controller, title remove from _Layout.cshtml, BSLCustomerPortalWeb.csproj - Kanchan

---
+++ BSLCustomerPortalWeb/Controllers/InquiryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BSLCustomerPortalWeb.Controllers
+{
+    public class InquiryController : Controller
+    {
+        // GET: Inquiry
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Quotation()
+        {
+            return View();
+        }
+
+        public ActionResult DisplayQuotation()
+        {
+            return View();
+        }
+
+    }
+}
