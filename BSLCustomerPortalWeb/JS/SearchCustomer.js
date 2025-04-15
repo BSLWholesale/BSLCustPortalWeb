fe3c32f7588ec82470d78dbbd68f7cac4ca1e7c7
Kanchan <banswaraws@gmail.com>
2025-04-15 18:48:01 +0530

    SearchCustomer.js, added JS file link Display_Schedule_Meeting.cshtml, CustomerMeetingListDetail.cshtml,  BSLCustomerPortalWeb.csproj - Kanchan

---
+++ BSLCustomerPortalWeb/JS/SearchCustomer.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    $("#txtCompanyName1").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: '/CustomerVisit/Fn_Get_SearchCustomerEmail',
+                type: "POST",
+                dataType: "json",
+                data: { Prefix: request.term },
+                success: function (data) {
+                    var obj = JSON.parse(data.message)
+                    response($.map(obj, function (item) {
+                        return { label: item.SearchKeyword, value: item.SearchKeyword };
+                    }));
+                }
+            });
+        },
+        select: function (event, ui) {
+            var str = ui.item.value;
+            var vCustList = str.split("--");
+        },
+        open: function (event, ui) {
+            $(".ui-autocomplete").css("z-index", "10000");
+        },
+        close: function (event, ui) {
+            $(".ui-autocomplete").css("z-index", "");
+        }
+    });
+});
